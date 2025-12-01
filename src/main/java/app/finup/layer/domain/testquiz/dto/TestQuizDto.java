93481e1b3122fd9a759f71f406f3922cab3cf74a
phj <phjcos@gmail.com>
2025-12-01 14:02:55 +0900

    개념 테스트 엔티티 작성

---
+++ src/main/java/app/finup/layer/domain/testquiz/dto/TestQuizDto.java
@@ -0,0 +1,13 @@
+package app.finup.layer.domain.testquiz.dto;
+
+import lombok.*;
+
+/**
+ * 개념 테스트 점수 내역 DTO 클래스
+ * @author phj
+ * @since 2025-12-01
+ */
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TestQuizDto {
+}
---
+++ src/main/java/app/finup/layer/domain/testquizrecord/dto/TestQuizRecordDto.java
@@ -0,0 +1,13 @@
+package app.finup.layer.domain.testquizrecord.dto;
+
+import lombok.*;
+
+/**
+ * 개념 테스트 질문 내역 DTO 클래스
+ * @author phj
+ * @since 2025-12-01
+ */
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TestQuizRecordDto {
+}
