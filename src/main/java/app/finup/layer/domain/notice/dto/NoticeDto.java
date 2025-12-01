674748d229d04eb07855909b1bfbcbedb81e1b4a
DarkLight0418 <samseong0418@naver.com>
2025-12-01 11:20:51 +0900

    공지사항(Notice) DTO 생성

---
+++ src/main/java/app/finup/layer/domain/notice/dto/NoticeDto.java
@@ -0,0 +1,31 @@
+package app.finup.layer.domain.notice.dto;
+
+import lombok.*;
+
+/**
+ * 공지사항 DTO 클래스
+ * @author khj
+ * @since 2025-12-01
+ */
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeDto {
+
+    /**
+     * 일반적인 단일(상세) 조회 결과로 사용
+     */
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Detail {
+        private Long noticeId;
+        private String title;
+        private String content;
+        private String admin;
+    }
+
+    /**
+     * 리스트(페이징) 결과로 사용
+     */
+}
