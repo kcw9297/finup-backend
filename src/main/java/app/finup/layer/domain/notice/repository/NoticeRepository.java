dedb4f91ec716a30814764924bdffd02328b00c9
DarkLight0418 <samseong0418@naver.com>
2025-12-01 14:10:36 +0900

    AppStatus 공지사항(Notice) 관련 항목 추가, 공지사항 서비스 구현체 생성, 레포지토리 커스텀 메소드 삽입

--- src/main/java/app/finup/layer/domain/notice/repository/NoticeRepository.java
+++ src/main/java/app/finup/layer/domain/notice/repository/NoticeRepository.java
@@ -1,8 +1,12 @@
 package app.finup.layer.domain.notice.repository;
 
+import app.finup.layer.domain.notice.dto.NoticeDto;
 import app.finup.layer.domain.notice.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+import java.util.List;
 
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> search(NoticeDto.Search rq);
+    Long searchCount(NoticeDto.Search rq);
 }
