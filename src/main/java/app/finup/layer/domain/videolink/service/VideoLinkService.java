6ad1cc270888b1f2946a575c7e9100b5ff80200e
kcw97 <kcw9297@gmail.com>
2025-12-02 16:12:06 +0900

    VideoLink 개발 2 CRUD 기능 서비스 메소드 추가

--- src/main/java/app/finup/layer/domain/videolink/service/VideoLinkService.java
+++ src/main/java/app/finup/layer/domain/videolink/service/VideoLinkService.java
@@ -11,16 +11,37 @@ import java.util.List;
  */
 public interface VideoLinkService {
 
+
     /**
      * 특정 단계 학습에 속하는 비디오 링크 목록 조회
      * @param studyId 단계 학습번호
      */
     List<VideoLinkDto.Summary> getListByStudy(Long studyId);
 
+
     /**
      * 페이지 홈에 속하는 비디오 링크 목록 조회
      */
     List<VideoLinkDto.Summary> getListByHome();
 
-    void add(VideoLinkDto.Add videoLinkDto);
+
+    /**
+     * 학습영상 링크 추가
+     * @param rq 영상 추가요청 DTO
+     */
+    void add(VideoLinkDto.Add rq);
+
+
+    /**
+     * 학습영상 링크 정보 변경 (정렬 순서를 바꾸지 않음)
+     * @param rq 영상 추가요청 DTO
+     */
+    void edit(VideoLinkDto.Edit rq);
+
+
+    /**
+     * 학습영상 순서 재정렬(변경)
+     * @param rq 영상 재정렬 요청 DTO
+     */
+    void reorder(VideoLinkDto.Reorder rq);
 }
