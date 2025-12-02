84a6fda42942c1f87751612b80f8a6375fc95fdd
kcw97 <kcw9297@gmail.com>
2025-12-02 19:19:57 +0900

    Study 개발 - StudyStatus 위치 변경 (MemberStudy 에서 취급 예정) 및 Study CRUD DTO 생성

--- src/main/java/app/finup/layer/domain/study/service/StudyService.java
+++ src/main/java/app/finup/layer/domain/study/service/StudyService.java
@@ -1,5 +1,6 @@
 package app.finup.layer.domain.study.service;
 
+import app.finup.common.dto.Page;
 import app.finup.layer.domain.study.dto.StudyDto;
 
 import java.util.List;
@@ -11,4 +12,13 @@ import java.util.List;
  */
 public interface StudyService {
 
+    Page<StudyDto.Row> getPagedList(StudyDto.Search rq);
+
+    StudyDto.Detail getDetail(Long studyId);
+
+    void add(StudyDto.Add rq);
+
+    void edit(StudyDto.Edit rq);
+
+    void remove(Long studyId);
 }
