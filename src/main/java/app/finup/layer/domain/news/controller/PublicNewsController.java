176c836f13ffeee8fb329676e166840fd10e50e6
dusgml4258-test <dusgml4258@gmail.com>
2025-12-02 20:10:57 +0900

    news ai 구현

--- src/main/java/app/finup/layer/domain/news/controller/PublicNewsController.java
+++ src/main/java/app/finup/layer/domain/news/controller/PublicNewsController.java
@@ -2,7 +2,9 @@ package app.finup.layer.domain.news.controller;
 
 import app.finup.common.constant.Url;
 import app.finup.common.utils.Api;
+import app.finup.layer.domain.news.service.NewsAiService;
 import app.finup.layer.domain.news.service.NewsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.Map;
 public class PublicNewsController {
 
     private final NewsService newsService;
+    private final NewsAiService newsAiService;
 
     @GetMapping("/list")
     public ResponseEntity<?> getNews(int page, String keyword, String category) {
@@ -29,5 +32,12 @@ public class PublicNewsController {
         return Api.ok(newsService.extractArticle(url));
     }
 
+    @GetMapping("/detail-ai")
+    public ResponseEntity<?> getArticleByAi(@RequestParam String url) throws JsonProcessingException {
+        String article = newsService.extractArticle(url);
+        Map<String,Object> ai = newsAiService.analyzeArticle(article);
+
+        return Api.ok(Map.of("content",article,"ai",ai));
+    }
 
 }
--- src/main/java/app/finup/layer/domain/news/service/NewsService.java
+++ src/main/java/app/finup/layer/domain/news/service/NewsService.java
@@ -3,8 +3,10 @@ package app.finup.layer.domain.news.service;
 import app.finup.layer.domain.news.dto.NewsDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface NewsService {
     List<NewsDto.Summary> getNews(int page, String keyword, String sort);
     String extractArticle(String url);
+
 }
