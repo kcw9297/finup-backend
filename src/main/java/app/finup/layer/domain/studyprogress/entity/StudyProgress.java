dae0766d0647e2d1991e5f721de7214449ad5f37
kcw97 <kcw9297@gmail.com>
2025-12-02 20:16:48 +0900

    StudyProgress 개발 - MemberStudy 에서 명칭 변경 후, 필요 DTO 및 Repository 메소드 생성

---
+++ src/main/java/app/finup/layer/domain/studyprogress/entity/StudyProgress.java
@@ -0,0 +1,76 @@
+package app.finup.layer.domain.studyprogress.entity;
+
+import app.finup.layer.base.entity.BaseEntity;
+import app.finup.layer.domain.member.entity.Member;
+import app.finup.layer.domain.studyprogress.enums.StudyStatus;
+import app.finup.layer.domain.study.entity.Study;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ * 회원이 진행 중인 학습상태 기록을 위한 엔티티 클래스
+ * @author kcw
+ * @since 2025-12-02
+ */
+
+@Entity
+@Table(name = "study_progress")
+@DynamicUpdate
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudyProgress extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberStudyId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StudyStatus studyStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member; // 학습 중인 회원
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "study_id", updatable = false)
+    private Study study;
+
+    @Builder
+    public StudyProgress(Member member, Study study) {
+        this.member = member;
+        this.study = study;
+        setDefault();
+    }
+
+    // 기본 값 설정
+    private void setDefault() {
+        this.studyStatus = StudyStatus.BEFORE;
+    }
+
+    /* 연관관계 메소드 */
+
+    /**
+     * 진행 상태로 변경
+     */
+    public void progress() {
+        this.studyStatus = StudyStatus.IN_PROGRESS;
+    }
+
+    /**
+     * 완료 상태로 변경
+     */
+    public void complete() {
+        this.studyStatus = StudyStatus.COMPLETED;
+    }
+}
+
+
