a3c458c55ea9c58b84ffd242bceee22576671968
dusgml4258-test <dusgml4258@gmail.com>
2025-12-04 13:07:16 +0900

    news redis 추가

--- src/main/java/app/finup/infra/redis/manager/RedisCacheManager.java
+++ src/main/java/app/finup/infra/redis/manager/RedisCacheManager.java
@@ -1,4 +1,12 @@
 package app.finup.infra.redis.manager;
 
 public interface RedisCacheManager {
+
+    void saveNews(String key, String json, long ttlMillis);
+
+    String getNews(String key);
+
+    void deleteNews(String key);
+
+    boolean exists(String key);
 }
