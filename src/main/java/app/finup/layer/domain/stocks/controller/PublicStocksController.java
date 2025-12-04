fe4880b872cf7ce5be2070d72068cadf20712048
rmadudbb <rmadudbb@gmail.com>
2025-12-04 17:52:07 +0900

    컨트롤러 주석

--- src/main/java/app/finup/layer/domain/stocks/controller/PublicStocksController.java
+++ src/main/java/app/finup/layer/domain/stocks/controller/PublicStocksController.java
@@ -8,6 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 종목+ 리스트 REST API 클래스
+ * @author lky
+ * @since 2025-12-03
+ */
+
 @Slf4j
 @RequestMapping(Url.STOCKS_PUBLIC)
 @RestController
@@ -21,6 +27,11 @@ public class PublicStocksController {
         return Api.ok(stocksService.getMarketCapRanking());
     }*/
 
+    /**
+     * 종목 상세페이지 조회 API
+     * [GET] stocks/detail/{code}
+     * @param code 종목코드
+     */
     @GetMapping("/detail/{code:[0-9]+}")
     public ResponseEntity<?> getDetail(@PathVariable String code) {
         return Api.ok(stocksService.getDetail(code));
