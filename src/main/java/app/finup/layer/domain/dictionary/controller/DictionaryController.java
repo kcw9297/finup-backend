81e906e6e3270b7049a0fb71ce121b29fe10a9b1
DarkLight0418 <samseong0418@naver.com>
2025-12-10 11:25:26 +0900

    PDF 관련 별도 메소드 생성(회원 목록 전체 조회)

---
+++ src/main/java/app/finup/layer/domain/dictionary/controller/DictionaryController.java
@@ -0,0 +1,14 @@
+package app.finup.layer.domain.dictionary.controller;
+
+import app.finup.common.constant.Url;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+//@RequestMapping(Url)
+@RequiredArgsConstructor
+public class DictionaryController {
+}
